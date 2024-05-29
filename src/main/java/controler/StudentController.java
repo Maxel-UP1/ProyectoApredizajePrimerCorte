0d00e5565823a1fd3515d25576ed0cba7fd40781
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2024-05-29 00:43:23 -0500

    Fix controler usuario, para uncio usuario que inicio sesion

--- src/main/java/controler/StudentController.java
+++ src/main/java/controler/StudentController.java
@@ -7,28 +7,27 @@ import model.Student;
 
 public class StudentController {
 
-	private List<Student> student_controler;
+	private Student studentActual;
 
 	 public StudentController() {
-		student_controler = new LinkedList<Student>();
+		studentActual = new Student();
 	}
 
 
 	
-	public boolean addStudent(Student student) {
-		boolean auxStudent = student_controler.add(student);
-		return auxStudent;
+	public void createStudent(int id_student, String name_student, String mail_student, int phone_student  ) {
+		studentActual.setId_student(id_student);
+		studentActual.setName_student(name_student);
+		studentActual.setMail_student(mail_student);
+		studentActual.setPhone_student(phone_student);
+
 	}
 	
 	
-	public Student deleteStudent(Student student) {
-		Student auxStudent = null;
-		for (int i = 0; i > student_controler.size(); i++) {
-			if(student_controler.get(i).getId_student()==student.getId_student()) {
-				auxStudent=student_controler.remove(i);
-			}
-		}
-		return auxStudent;
+	public void deleteStudent() {
+		studentActual = null;
 	}
-	
+
+
+
 }
