f4a980983b36f2f43066cad4a69e44f1703174d1
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2024-05-28 23:33:03 -0500

    Base funcional vista incial

--- src/main/java/controlerView/UserViewInitialController.java
+++ src/main/java/controlerView/UserViewInitialController.java
@@ -1,5 +1,39 @@
 package controlerView;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
 public class UserViewInitialController {
 
+    public Label bienvenidoLabel;
+    public Label NameLabel;
+    public Label matematicasLabel;
+    public Label programacionLabel;
+    public ComboBox comboBoxMatematicas;
+    public ComboBox comboBoxProgramación;
+    public Button goBtn;
+    public TableColumn curseMathColumb;
+    public TableColumn descriptionMathColumb;
+    public TableColumn itrScoreMathColumb;
+    public Button sendbtn;
+    public TableColumn curseProgrammingColumb1;
+    public TableColumn descriptionProgrammingColumb1;
+    public TableColumn itrScoreProgrammingColumb1;
+
+
+
+    public void selecDificultyMath(ActionEvent actionEvent) {
+    }
+
+    public void selecDificultyProgramming(ActionEvent actionEvent) {
+    }
+
+    public void searchCourse(ActionEvent actionEvent) {
+    }
+
+    public void sendTree(ActionEvent actionEvent) {
+    }
 }
