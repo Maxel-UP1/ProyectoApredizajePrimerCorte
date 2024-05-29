f34f4ad6acd4f3aa64bdd690cdcd3ab597c11575
mayerly.cuida@uptc.edu.co <mayerly.cuida@uptc.edu.co>
2024-05-29 00:30:04 -0500

    CONTROLLER

--- src/main/java/controler/StudentController.java
+++ src/main/java/controler/StudentController.java
@@ -1,5 +1,34 @@
 package controler;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import model.Student;
+
 public class StudentController {
 
+	private List<Student> student_controler;
+
+	 public StudentController() {
+		student_controler = new LinkedList<Student>();
+	}
+
+
+	
+	public boolean addStudent(Student student) {
+		boolean auxStudent = student_controler.add(student);
+		return auxStudent;
+	}
+	
+	
+	public Student deleteStudent(Student student) {
+		Student auxStudent = null;
+		for (int i = 0; i > student_controler.size(); i++) {
+			if(student_controler.get(i).getId_student()==student.getId_student()) {
+				auxStudent=student_controler.remove(i);
+			}
+		}
+		return auxStudent;
+	}
+	
 }
--- src/main/java/controler/UserControlers.java
+++ src/main/java/controler/UserControlers.java
@@ -1,18 +1,44 @@
 package controler;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import model.User;
 
 public class UserControlers {
-	
-	private User user_controler;
-	
-	
-	
+
+	private List<User> user_controler;
+
+	public UserControlers() {
+		user_controler = new LinkedList<User>();
+	}
+
 	public boolean validarUser(User user) {
-		if(user.getUser().equals(user_controler.getUser())&&user.getPassword().equals(user_controler.getPassword())){
-			return true;
-		}else{
+		for (int i = 0; i > user_controler.size(); i++) {
+			if (user.getUser().equals(user_controler.get(i).getUser())
+					&& user.getPassword().equals(user_controler.get(i).getPassword())) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 		return false;
 	}
- }
+	
+	public boolean addUser(User user) {
+		boolean auxUser = user_controler.add(user);
+		return auxUser;
+	}
+	
+	public User deleteUser(User user) {
+		User auxUser = null;
+		for (int i = 0; i > user_controler.size(); i++) {
+			if(user_controler.get(i).getUser().equals(user.getUser())) {
+				auxUser=user_controler.remove(i);
+			}
+		}
+		return auxUser;
+	}
+	
+
 }
